17c3e7ad5621140d65a328d9b442617468e515fc
Daniel Calderon <Daniel.calderon.e.v@gmail.com>
2021-07-22 14:42:47 -0300

    Se creo el register persona

--- persona.php
+++ persona.php
@@ -2,13 +2,19 @@
 // Initialize the session
 session_start();
 
-// Check if the user is logged in, if not then redirect him to login page
+// Check if the user is logged in, otherwise redirect to login page
 if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     header("location: login.php");
     exit;
 }
-?>
 
+// Include config file
+require_once "connectionBD.php";
+
+// Define variables and initialize with empty values
+$new_password = $confirm_password = "";
+$new_password_err = $confirm_password_err = "";
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,9 +22,14 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin laboratorio clinico</title>
     <link href="" rel="stylesheet" type="text/css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Nueva persona</title>
+
     <style type="text/css">
         body {
             font: 14px sans-serif;
@@ -44,11 +55,11 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                        <a class="nav-link active " aria-current="page" href="home.php">Inicio</a>
+                    <li class="nav-item">
+                        <a class="nav-link " aria-current="page" href="home.php">Inicio</a>
                     </li>
-                <li class="nav-item">
-                        <a class="nav-link " aria-current="page" href="persona.php">Registrar</a>
+                    <li class="nav-item">
+                        <a class="nav-link active " aria-current="page" href="persona.php">Registrar</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -61,7 +72,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                     <li class="nav-item">
                         <a class="nav-link" aria-current="page" href="logout.php">Cerrar sesión</a>
                     </li>
-                 
+
                 </ul>
                 <form class="d-flex">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
@@ -70,7 +81,6 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
             </div>
         </div>
     </nav>
-  
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 
--- reset-password.php
+++ reset-password.php
@@ -1,76 +1,216 @@
 <?php
 // Initialize the session
 session_start();
- 
+
 // Check if the user is logged in, otherwise redirect to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     header("location: login.php");
     exit;
 }
- 
+
 // Include config file
 require_once "connectionBD.php";
- 
+
 // Define variables and initialize with empty values
 $new_password = $confirm_password = "";
 $new_password_err = $confirm_password_err = "";
- 
+
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     // Validate new password
-    if(empty(trim($_POST["new_password"]))){
-        $new_password_err = "Please enter the new password.";     
-    } elseif(strlen(trim($_POST["new_password"])) < 6){
+    if (empty(trim($_POST["new_password"]))) {
+        $new_password_err = "Please enter the new password.";
+    } elseif (strlen(trim($_POST["new_password"])) < 6) {
         $new_password_err = "La contraseña al menos debe tener 6 caracteres.";
-    } else{
+    } else {
         $new_password = trim($_POST["new_password"]);
     }
-    
+
     // Validate confirm password
-    if(empty(trim($_POST["confirm_password"]))){
+    if (empty(trim($_POST["confirm_password"]))) {
         $confirm_password_err = "Por favor confirme la contraseña.";
-    } else{
+    } else {
         $confirm_password = trim($_POST["confirm_password"]);
-        if(empty($new_password_err) && ($new_password != $confirm_password)){
+        if (empty($new_password_err) && ($new_password != $confirm_password)) {
             $confirm_password_err = "Las contraseñas no coinciden.";
         }
     }
-        
+
     // Check input errors before updating the database
-    if(empty($new_password_err) && empty($confirm_password_err)){
+    if (empty($new_password_err) && empty($confirm_password_err)) {
         // Prepare an update statement
         $sql = "UPDATE users SET password = ? WHERE id = ?";
-        
-        if($stmt = mysqli_prepare($link, $sql)){
+
+        if ($stmt = mysqli_prepare($link, $sql)) {
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "si", $param_password, $param_id);
-            
+
             // Set parameters
             $param_password = password_hash($new_password, PASSWORD_DEFAULT);
             $param_id = $_SESSION["id"];
-            
+
             // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
+            if (mysqli_stmt_execute($stmt)) {
                 // Password updated successfully. Destroy the session, and redirect to login page
                 session_destroy();
                 header("location: login.php");
                 exit();
-            } else{
+            } else {
                 echo "Algo salió mal, por favor vuelva a intentarlo.";
             }
         }
-        
+
         // Close statement
         mysqli_stmt_close($stmt);
     }
-    
+
     // Close connection
     mysqli_close($link);
 }
 ?>
- 
+
 <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin laboratorio clinico</title>
+    <link href="" rel="stylesheet" type="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style type="text/css">
+        body {
+            font: 14px sans-serif;
+            text-align: center;
+            overflow: hidden;
+
+        }
+
+        .container-level {
+            margin-top: 20px;
+        }
+
+        .wrapper {
+            width: 500px;
+            padding: 20px;
+        }
+
+        .card {
+            width: 500px;
+            height: 555px;
+            overflow-y: hidden;
+            overflow-x: hidden;
+            border: 2px solid rgba(0, 0, 0, .125);
+            background-color: #f5f5f5;
+        }
+
+        h1 {
+            font-size: 20px;
+        }
+
+        h3 {
+            font-size: 14px;
+            font-weight: bold;
+            position: absolute;
+            top: 20%;
+            right: 70%;
+        }
+
+        .confir{
+            font-size: 14px;
+            font-weight: bold;
+            position: absolute;
+            bottom: 100%;
+            right: 70%;
+        }
+
+        .bg-primary {
+            background-color: #6f97d2 !important;
+        }
+
+        .border-prima {
+            border-color: #6f97d2;
+        }
+        .buttonEnviar {
+            position: absolute;
+            top: 78%;
+            right: 80%;
+        }
+        .password {
+            position: absolute;
+            bottom: 30%;
+            width: 88%;
+        }
+
+        .password-confir{
+            position: absolute;
+            bottom: 50%;
+            width: 88%;
+        }
+
+        .buttonCancel {
+            position: absolute;
+            top: 78%;
+            right: 60%;
+        }
+    </style>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <h1><b>@<?php echo htmlspecialchars($_SESSION["username"]); ?></b></h1>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="logout.php">Cerrar sesión</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container-level">
+        <div class="card bg-transparent border-prima">
+            <div class="wrapper">
+                <div class="card-header  bg-transparent border-prima">
+                    <h2>Cambiar contraseña</h2>
+                </div>
+                <p>Complete este formulario para restablecer su contraseña.</p>
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                    <div class="form-group password-confir  <?php echo (!empty($new_password_err)) ? 'has-error' : ''; ?>">
+                        <label><h3> Nueva Contraseña:</h3></label>
+                        <input type="password" name="new_password" class="form-control g-transparent border-prima" value="<?php echo $new_password; ?>">
+                        <span class="help-block"><?php echo $new_password_err; ?></span>
+                    </div>
+                    <div class="form-group  password <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+                        <label class="confir">Confirmar contraseña</label>
+                        <input type="password" name="confirm_password" class="form-control g-transparent border-prima">
+                        <span class="help-block"><?php echo $confirm_password_err; ?></span>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-primary buttonEnviar" value="Enviar">
+                        <a class="btn btn-link buttonCancel" href="home.php">Cancelar</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
+
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -98,9 +238,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             </div>
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Enviar">
-                <a class="btn btn-link" href="welcome.php">Cancelar</a>
+                <a class="btn btn-link" href="home.php">Cancelar</a>
             </div>
         </form>
     </div>    
 </body>
-</html>
+</html> -->
